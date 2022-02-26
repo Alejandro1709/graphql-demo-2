603df6b9f3916a73d8862263b31bb71720f0bff7
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-02-26 13:18:42 -0500

    create graphql server

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema.js';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+  })
+);
 
 app.listen(4000, () => {
   console.log('Server is live at: http://localhost:4000');
